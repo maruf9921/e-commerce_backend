63c9d04ff92b048af9a116000710e35928702a08
dipto-roy <aiubdip@gmail.com>
2025-07-30 02:01:43 +0600

    basic pipe done,

--- src/seller/dto/seller.dto.ts
+++ src/seller/dto/seller.dto.ts
@@ -1,9 +1,10 @@
 import { match } from 'assert';
-import{ IsString, Matches, MinLength }from 'class-validator';
+import{ IsNotEmpty, IsString, Matches, MinLength }from 'class-validator';
 
 export class SellerDto {
 
     //Name validation
+    @IsNotEmpty()
     @Matches(/^[a-zA-Z0-9 ]+$/, {
         message: 'Name must not contain any special characters!' ,
     })
@@ -12,19 +13,23 @@ export class SellerDto {
     
    
     //Password validation
+    @IsNotEmpty()
     @MinLength(6, {
         message: 'Password must be at least 6 characters long!' ,
     })
     @Matches(/[a-z]/, {
         message: 'Password must contain at least one lowercase letter!' ,
     })
+    @IsString()
     password: string;
 
     //Phone number validation
+    @IsNotEmpty()
     @Matches(/^01\d+$/, {
     message: 'Phone number feild must start with 01 !' ,
 
    })
+    @IsString()
     phone: string;
 
 
